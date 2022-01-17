0c4ae61bf416f394c2a70ed3d8b69304b82bd38a
Tim Korb <tim-korb@gmx.de>
2022-01-17 10:14:35 +0100

    VarRectangle added, Point to Public, Program added VarRectangle to SimpeImage

--- Program.cs
+++ Program.cs
@@ -2,100 +2,110 @@
 using ImageTools;
 using SkiaSharp;
 
+
 namespace Grafikprogramm
 {
-  class Program
-  {
-
-    public static int DefaultHeight = 400;
-
-    public static int DefaultWidth = 300;
-
-    static void Main(string[] args)
+    class Program
     {
-      SimpleImage();
-      //RandomImage();
-    }
-    
 
-    private static void SimpleImage()
-    {
-      Canvas canvas = new Canvas(DefaultHeight, DefaultWidth);
-      Circle circle1 = new Circle(new SKColor(255, 0, 219),
-          SKColors.Transparent,
-          50, 100,
-          15);
-      Rectangle rectangle1 = new Rectangle(
-          SKColors.Chocolate,
-          SKColors.Transparent,
-          30, 40,
-          25, 30);
-      Triangle triangle1 = new Triangle(
-          SKColors.LightGray,
-          SKColors.Transparent,
-          new Point(20, 50),
-          new Point(200, 250),
-          new Point(20, 200)
-      );
-      canvas.AddShape(circle1);
-      canvas.AddShape(triangle1);
-      canvas.AddShape(rectangle1);
-      string imagePath = "simple_image.png";
-      ImageUtil.SavePNG(canvas, imagePath);
-      ImageUtil.DisplayImage(imagePath);
-    }
+        public static int DefaultHeight = 400;
 
-    private static void RandomImage()
-    {
-      int imageHeight = 1080;
-      int imageWidth = 720;
-      Random r = new Random();
-      Canvas canvas = new Canvas(imageHeight, imageWidth);
-      for (int i = 0; i < 50; i++)
-      {
-        int shapeType = r.Next(3);
-        IImage shape;
-        if (shapeType == 0)
+        public static int DefaultWidth = 300;
+
+        static void Main(string[] args)
         {
-          shape = new Triangle(
-             GetRandomColor(),
-             SKColors.Transparent,
-             new Point(r.Next(imageHeight), r.Next(imageWidth)),
-             new Point(r.Next(imageHeight), r.Next(imageWidth)),
-             new Point(r.Next(imageHeight), r.Next(imageWidth))
-          );
+            SimpleImage();
+            //RandomImage();
         }
-        else if (shapeType == 1)
+
+
+        private static void SimpleImage()
         {
-          shape = new Circle(
-              GetRandomColor(),
-              SKColors.Transparent,
-              r.Next(imageHeight), r.Next(imageWidth),
-              r.Next(100)
-          );
+            Canvas canvas = new Canvas(DefaultHeight, DefaultWidth);
+            Circle circle1 = new Circle(new SKColor(255, 0, 219),
+                SKColors.Transparent,
+                50, 100,
+                15);
+            Rectangle rectangle1 = new Rectangle(
+                SKColors.Chocolate,
+                SKColors.Transparent,
+                30, 40,
+                25, 30);
+            Triangle triangle1 = new Triangle(
+                SKColors.LightGray,
+                SKColors.Transparent,
+                new Point(20, 50),
+                new Point(200, 250),
+                new Point(20, 200));
+            VarRectangle varRectangle = new VarRectangle(
+                SKColors.Green,
+                SKColors.Transparent,
+                new Point(100, 10),
+                new Point(200, 250),
+                new Point(200, 200),
+                new Point(20, 0)
+            );
+
+            canvas.AddShape(circle1);
+            canvas.AddShape(triangle1);
+            canvas.AddShape(rectangle1);
+            canvas.AddShape(varRectangle);
+            string imagePath = "simple_image.png";
+            ImageUtil.SavePNG(canvas, imagePath);
+            ImageUtil.DisplayImage(imagePath);
         }
-        else
+
+        private static void RandomImage()
         {
-          shape = new Rectangle(
-              GetRandomColor(),
-              SKColors.Transparent,
-              r.Next(imageHeight), r.Next(imageWidth),
-              r.Next(imageHeight), r.Next(imageWidth)
-          );
+            int imageHeight = 1080;
+            int imageWidth = 720;
+            Random r = new Random();
+            Canvas canvas = new Canvas(imageHeight, imageWidth);
+            for (int i = 0; i < 50; i++)
+            {
+                int shapeType = r.Next(3);
+                IImage shape;
+                if (shapeType == 0)
+                {
+                    shape = new Triangle(
+                       GetRandomColor(),
+                       SKColors.Transparent,
+                       new Point(r.Next(imageHeight), r.Next(imageWidth)),
+                       new Point(r.Next(imageHeight), r.Next(imageWidth)),
+                       new Point(r.Next(imageHeight), r.Next(imageWidth))
+                    );
+                }
+                else if (shapeType == 1)
+                {
+                    shape = new Circle(
+                        GetRandomColor(),
+                        SKColors.Transparent,
+                        r.Next(imageHeight), r.Next(imageWidth),
+                        r.Next(100)
+                    );
+                }
+                else
+                {
+                    shape = new Rectangle(
+                        GetRandomColor(),
+                        SKColors.Transparent,
+                        r.Next(imageHeight), r.Next(imageWidth),
+                        r.Next(imageHeight), r.Next(imageWidth)
+                    );
 
+                }
+                canvas.AddShape(shape);
+            }
+            string imagePath = "random_image.png";
+            ImageUtil.SavePNG(canvas, imagePath);
+            ImageUtil.DisplayImage(imagePath);
         }
-        canvas.AddShape(shape);
-      }
-      string imagePath = "random_image.png";
-      ImageUtil.SavePNG(canvas, imagePath);
-      ImageUtil.DisplayImage(imagePath);
-    }
 
-    private static Random r = new Random();
+        private static Random r = new Random();
 
-    private static SKColor GetRandomColor()
-    {
-      return new SKColor((byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256));
+        private static SKColor GetRandomColor()
+        {
+            return new SKColor((byte)r.Next(256), (byte)r.Next(256), (byte)r.Next(256));
+        }
     }
-  }
 }
---
+++ VarRectangle.cs
@@ -0,0 +1,64 @@
+﻿using ImageTools;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Grafikprogramm
+{
+    public class VarRectangle : IImage
+    {
+        Point A, B, C, D;
+        private readonly SKColor ForegroundColor = SKColors.Red;
+
+        private readonly SKColor BackgroundColor = SKColors.LightGreen;
+        public VarRectangle(SKColor foregroundColor, SKColor backgroundColor, Point a, Point b, Point c, Point d)
+        {
+            ForegroundColor = foregroundColor;
+            BackgroundColor = backgroundColor;
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+        public bool IsRightToLine(Point lstart, Point lend, Point x)
+        {
+            int v1X = lend.X - lstart.X;
+            int v1Y = lend.Y - lstart.Y;
+
+            int v2X = lend.X - x.X;
+            int v2Y = lend.Y - x.Y;
+
+            int res = (v1X * v2Y) - (v1Y * v2X);
+            return res <= 0;
+        }
+
+        public SKColor GetPixelColor(int x, int y)
+        {
+            Point p = new Point(x, y);
+            if (IsRightToLine(A, B, p)
+                &&
+                IsRightToLine(B, C, p)
+                &&
+                IsRightToLine(C, D, p)
+                &&
+                IsRightToLine(D, A, p)
+                )
+            {
+                return ForegroundColor;
+            }
+            return BackgroundColor;
+        }
+
+        public int Height()
+        {
+            return 300;
+        }
+
+        public int Width()
+        {
+            return 400;
+        }
+    }
+}
